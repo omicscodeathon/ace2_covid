9533018455fe4257c69639d7da54f99adbf8e91f
marionnyaboke <45264074+marionnyaboke@users.noreply.github.com>
2022-05-27 23:25:23 +0300

    Update 02_generate-fastqc-reports.sh

--- scripts/02_generate-fastqc-reports.sh
+++ scripts/02_generate-fastqc-reports.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd /home/nyamarim/qualitycontrol
+cd /srv/data/my_shared_data_folder/ace2covid/results/qualitycontrol
 
 #Script to generate FastQC reports using the FastQC tool
 
 # fastq files directory
-FASTQ_DIR="/home/nyamarim/data/healthy"
+FASTQ_DIR="/srv/data/my_shared_data_folder/ace2covid/raw"
 
 # fastqc reports directory
-REPORT_DIR="/home/nyamarim/qualitycontrol"
+REPORT_DIR="/srv/data/my_shared_data_folder/ace2covid/results/qualitycontrol"
 
 for file in $FASTQ_DIR/*.fastq; do
    fastqc ${file} -o ${REPORT_DIR}
