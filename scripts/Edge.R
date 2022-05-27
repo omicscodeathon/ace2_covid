7a5bde63f7e5b12f45f2e2e9c306a40c20c71845
marionnyaboke <45264074+marionnyaboke@users.noreply.github.com>
2022-05-27 23:49:21 +0300

    Update Edge.R

--- scripts/Edge.R
+++ scripts/Edge.R
@@ -3,18 +3,19 @@ library(limma)
 library(RColorBrewer)
 library(mixOmics)
 library(HTSFilter)
-library(ggplot2) #Best plots
+library(ggplot2)
 library(ggrepel)
 library(dplyr)
 
+
 # set the directory from which files are imported
-directory <- "C:/Users/user/Desktop/marion/ACE2/"
+directory <- "C:/Users/Marion/Desktop/ACE/african-vs-non-african/"
 dir(directory)
 
 # read files
 rawCountTable <- read.table(paste0(directory,"reads_count.txt"), header=TRUE,
                             row.names=1, sep = "\t")
-sampleInfo <- read.table(paste0(directory,"metadata.txt"), header=TRUE,
+sampleInfo <- read.table(paste0(directory,"Metadata.txt"), header=TRUE,
                          row.names=1, sep = "\t")
 
 head(rawCountTable)
@@ -26,9 +27,19 @@ head(rawCountTable)
 colnames(rawCountTable)
 rownames(sampleInfo)
 
-# create the ‘condition’ column
-condition = c("african", "non-african")
+# create the â€˜conditionâ€™ column
+condition = c("african", "african", "african", "african", "african", "african", "african", 
+              "african", "african", "african", "african", "african", "african", "african",
+              "non-african", "non-african", "non-african", "non-african", "non-african", "non-african",
+              "non-african", "non-african", "non-african", "non-african", "non-african", "non-african",
+              "non-african", "non-african", "non-african", "non-african", "non-african", "non-african", 
+              "non-african", "non-african", "non-african", "non-african", "non-african", "non-african",
+              "non-african", "non-african", "non-african", "non-african", "non-african", "non-african",
+              "non-african", "non-african", "non-african")
+
 sampleInfo = data.frame(sampleInfo, condition)
+
+#sampleInfo = subset(sampleInfo, select= -condition)
 sampleInfo$condition
 
 # create a DGEList data object
@@ -52,7 +63,7 @@ pseudoCounts <- log2(dge$counts+1)
 head(pseudoCounts)
 
 # histogram for pseudo-counts
-hist(pseudoCounts[,"SRR15042704"])
+hist(pseudoCounts[,"SRR13081202"])
 
 # boxplot for pseudo-counts
 boxplot(pseudoCounts, col="cyan")
@@ -80,14 +91,15 @@ head(dge$counts)
 
 # estimate the normalization factors
 dge <- calcNormFactors(dge, method="TMM")
-dge$samples
+dge$samples$group <- relevel(dge$samples$group, ref="non-african") #sets non-african as the control group
+dge$samples$group
 
 # estimate common and tagwise dispersion
 dge <- estimateCommonDisp(dge)
 dge <- estimateTagwiseDisp(dge)
 dge
 
-# perform an exact test for the difference in expression between the two conditions “treated” and “control”
+# perform an exact test for the difference in expression between the conditions african and non-african
 dgeTest <- exactTest(dge)
 dgeTest
 
@@ -113,10 +125,10 @@ hist(dgeTestFilt$table[,"PValue"], breaks=50)
 resNoFilt <- topTags(dgeTest, n=nrow(dgeTest$table))
 head(resNoFilt)
 
-resFilt <- topTags(dgeTestFilt, n=nrow(dgeTest$table))
-head(resFilt)
+#resFilt <- topTags(dgeTestFilt, n=nrow(dgeTest$table))
+#head(resFilt)
 
-rownames(resFilt)
+#rownames(resFilt)
 
 #resFilt$genelabels <- factor(resFilt$gene, levels = c("H2BC5", "CCNF", "H2BC21", "H3C4"))
 #resFilt$
@@ -128,7 +140,7 @@ sum(resNoFilt$table$FDR < 0.05)
 sum(resFilt$table$FDR < 0.05)
 
 # extract and sort differentially expressed genes
-sigDownReg <- resFilt$table[resFilt$table$FDR<0.05,]
+sigDownReg <- resNoFilt$table[resNoFilt$table$FDR<0.05,]
 sigDownReg <- sigDownReg[order(sigDownReg$logFC),]
 head(sigDownReg)
 
@@ -140,26 +152,27 @@ write.csv(sigDownReg, file="sigDownReg.csv")
 write.csv(sigUpReg, file="sigUpReg.csv")
 
 # Interpreting the DE analysis results
-# create a MA plot with 10% differentially expressed genes
+# create a MA plot with 5% differentially expressed genes
 
 plotSmear(dgeTestFilt,
-          de.tags = rownames(resFilt$table)[which(resFilt$table$FDR<0.10)])
+          de.tags = rownames(resNoFilt$table)[which(resNoFilt$table$FDR<0.05)])
 
 # create a Volcano plot
 
 # create a column with thresholds
-significance = -log10(resFilt$table$FDR) < 1
-resFilt$table$significance <- significance
-dim(resFilt$table)
+significance = -log10(resNoFilt$table$FDR) < 1
+resNoFilt$table$significance <- significance
+dim(resNoFilt$table)
 
 ## setting the values
-resFilt$table$diffexpressed <- "No"
+resNoFilt$table$diffexpressed <- "No"
 
 # if logFC > 0.1 and FDR < 0.05, set as "UP"
-resFilt$table$diffexpressed[resFilt$table$logFC > 1 & resFilt$table$FDR < 0.1] <- "Up"
+resNoFilt$table$diffexpressed[resNoFilt$table$logFC > 0.1 & resNoFilt$table$FDR < 0.05] <- "Up"
 
 # if log2Foldchange < -0.6 and pvalue < 0.05, set as "DOWN"
-resFilt$table$diffexpressed[resFilt$table$logFC < -1 & resFilt$table$FDR < 0.1] <- "Down"
+resNoFilt$table$diffexpressed[resNoFilt$table$logFC < -1 & resNoFilt$table$FDR < 0.05] <- "Down"
+
 # set different colors
 mycolors <- c("cornflowerblue", "black", "firebrick")
 names(mycolors) <- c("Down", "No", "Up")
@@ -167,16 +180,16 @@ names(mycolors) <- c("Down", "No", "Up")
 dev.off()
 
 
-input <- cbind(gene=rownames(resFilt$table), resFilt$table) #convert the rownames to a column
+input <- cbind(gene=rownames(resNoFilt$table), resNoFilt$table) #convert the rownames to a column
 volc = ggplot(input, aes(logFC, -log10(PValue))) + #volcanoplot with logFC versus pvalue
   geom_point() +
   theme_classic()+
-  geom_vline(xintercept=c(-1.5, 1.8), col="cyan", linetype="dashed") +
-  geom_hline(yintercept=-log10(0.002), col="cyan" , linetype="dashed")+
+  geom_vline(xintercept=c(-1.0, 0.8), col="orange", linetype="dashed") +
+  geom_hline(yintercept=-log10(0.005), col="orange" , linetype="dashed")+
   geom_point(aes(col=diffexpressed)) + #add points colored by significance
   scale_color_manual(values= mycolors) + 
-  ggtitle("African vs non- african") #e.g. 'Volcanoplot DESeq2'
-volc+geom_text_repel(data=head(input, 40), aes(label=gene), size = 2) #adding text for the top 20 genes
+  ggtitle("african vs non-african") #e.g. 'Volcanoplot DESeq2'
+volc+geom_text_repel(data=head(input, 1500), aes(label=gene), size = 3) #adding text for the top 20 genes
 #ggsave("Volcanoplot.jpeg", device="jpeg") #In case you want to easily save to disk
 
 
@@ -185,9 +198,8 @@ y <- cpm(dge, log=TRUE, prior.count = 1)
 head(y)
 
 # select 1% differentially expressed genes and produce a heatmap
-selY <- y[rownames(resFilt$table)[resFilt$table$FDR<0.01 & 
-                                    abs(resFilt$table$logFC)>1.5],]
-head(selY)
+selY <- y[rownames(resNoFilt$table)[resNoFilt$table$FDR<0.05 & 
+                                    abs(resNoFilt$table$logFC)>1.5],]
 
 cimColor <- colorRampPalette(rev(brewer.pal(9, "Blues")))(255)[255:1]
-cim(selY, margins = c(10, 10))
+cim(selY, margins = c(10, 5))
